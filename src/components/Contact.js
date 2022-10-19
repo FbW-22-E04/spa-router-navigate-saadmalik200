6b3b5e836b664c49f5bfc05ed78a9ce1ddea34fd
Saad Malik <saad_malik200@hotmail.com>
2022-10-19 14:35:23 +0200

    ex1

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const Contact = () => {
-  return <h1>Contact</h1>;
+  const navigate = useNavigate();
+  const changeHandle = () => {
+    navigate("/home");
+  };
+
+  return (
+    <div>
+      <h1>Contact</h1>
+      <button onClick={changeHandle}>Change Page</button>
+    </div>
+  );
 };
 
 export default Contact;
